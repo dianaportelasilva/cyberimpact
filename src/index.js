bbda757a0e264c89febb66a524bf5e300e0f4d9a
Antonio Almeida <promatik@gmail.com>
2022-07-13 10:21:02 +0100

    Added onInput event listeners

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { query } from "cantil";
+import { query, onDomReady } from "cantil";
 
-let form = query("form");
+const form = query("form");
+const locale = navigator.language ?? 'pt-PT';
+const currencyFormat = new Intl.NumberFormat(locale, { style: 'currency', currency: 'EUR' });
+const percentFormat = new Intl.NumberFormat(locale, { style: 'percent', minimumFractionDigits: 2 });
 
-form.query("button").addEventListener("click", (event) => {
+const formClick = (event) => {
   event.preventDefault();
 
   // validate form
@@ -11,24 +14,32 @@ form.query("button").addEventListener("click", (event) => {
     return form.reportValidity();
   }
 
-  // get form values
-  let cost = Number(form.query("#cost").value);
-  let volume = Number(form.query("#volume").value);
-  let weekdays = Number(form.query("#days").value);
-  let proporcion = Number(form.query("#proporcion").value) / 100;
-  let population = Number(form.query("#population").value);
+  calculate();
 
-  let currencyFormat = new Intl.NumberFormat('pt-PT', { style: 'currency', currency: 'EUR' });
+  // show results
+  query(".alert").classList.remove("d-none");
+}
+
+const calculate = () => {
+  // get form values
+  let cost = Number(form.query('#cost').value);
+  let volume = Number(form.query('#volume').value);
+  let weekdays = Number(form.query('#days').value);
+  let proporcion = Number(form.query('#proporcion').value) / 100;
+  let population = Number(form.query('#population').value);
+
   let affectedDays = proporcion * weekdays;
   let affectedCapacity = affectedDays / (weekdays === 5 ? 250 : 365);
   let costEstimated = affectedCapacity * cost;
   let costPerPerson = costEstimated / population;
 
-  query("[affected-days]").innerText = affectedDays;
-  query("[affected-capacity]").innerText = affectedCapacity;
+  query("[affected-days]").innerText = percentFormat.format(affectedDays / 100);
+  query("[affected-capacity]").innerText = percentFormat.format(affectedCapacity / 100);
   query("[cost-estimated]").innerText = currencyFormat.format(costEstimated);
   query("[cost-per-person]").innerText = currencyFormat.format(costPerPerson);
+}
 
-  // show results
-  query(".alert").classList.remove("d-none");
+onDomReady().then(() => {
+  form.queryAll('input, select').forEach(e => e.addEventListener('input', calculate));
+  form.query("button").addEventListener("click", formClick);
 });
